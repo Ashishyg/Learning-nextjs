3d574392eb790f1918fea7bb3907f6dae8c61be2
Ashish Y G <ashishyg9@gmail.com>
2024-08-03 13:05:53 +0530

    changes updated

--- ecommerce-website/src/app/page.jsx
+++ ecommerce-website/src/app/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '@/components/Button';
 
 const page = () => {
   return (
@@ -8,6 +9,7 @@ const page = () => {
       <p className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:from-inherit rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30 hover:bg-green-500">Hey There!, I am a student at SJEC and my hobbies are gaming, solving puzzles and watching murder mystery genre movies and series </p>
       <p className='text-sm text-white p-4 m-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg shadow-md'>I have understood everything learnt so far this week, no complaints at all</p>
       </div>
+      <Button/>
     |</>
   )
 }
